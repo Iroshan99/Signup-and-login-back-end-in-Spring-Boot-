e025bd9fa78538a07e6f73553275880634eb7f53
Iroshan99 <ampiamarakoon@std.appsc.sab.ac.lk>
2024-12-15 14:24:57 +0530

    testing1

--- src/main/java/com/example/signup/and/login/SignupAndLoginApplication.java
+++ src/main/java/com/example/signup/and/login/SignupAndLoginApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SignupAndLoginApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SignupAndLoginApplication.class, args);
+		System.out.println("spring boot log in and sign up");
 	}
 
 }
